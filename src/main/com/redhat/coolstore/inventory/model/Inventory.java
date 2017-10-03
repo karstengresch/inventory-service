5f244cf84c45c7574856d614aeff095ab14e7737
Karsten Gresch <git-2015@by-doing.org>
2017-10-03 12:02:56 +0200

    Going w/ field based only

--- src/main/com/redhat/coolstore/inventory/model/Inventory.java
+++ src/main/com/redhat/coolstore/inventory/model/Inventory.java
@@ -1,54 +1,22 @@
 package main.com.redhat.coolstore.inventory.model;
 
+import javax.persistence.Id;
+
 /**
  * Created by Karsten Gresch on 03.10.17.
  */
+
 public class Inventory
 {
+  @Id
   private String itemId;
+
   private String location;
+
   private Integer quantity;
+
   private String link;
 
-  
 
-  public String getItemId()
-  {
-    return itemId;
-  }
 
-  public void setItemId(String itemId)
-  {
-    this.itemId = itemId;
-  }
-
-  public String getLocation()
-  {
-    return location;
-  }
-
-  public void setLocation(String location)
-  {
-    this.location = location;
-  }
-
-  public Integer getQuantity()
-  {
-    return quantity;
-  }
-
-  public void setQuantity(Integer quantity)
-  {
-    this.quantity = quantity;
-  }
-
-  public String getLink()
-  {
-    return link;
-  }
-
-  public void setLink(String link)
-  {
-    this.link = link;
-  }
 }
