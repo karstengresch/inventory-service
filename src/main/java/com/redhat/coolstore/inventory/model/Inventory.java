48d40dfc79fcf5f88452af5d826d11ebf9a42215
Karsten Gresch <git-2015@by-doing.org>
2017-10-03 12:12:41 +0200

    Added JPA persistence file

---
+++ src/main/java/com/redhat/coolstore/inventory/model/Inventory.java
@@ -0,0 +1,31 @@
+package main.com.redhat.coolstore.inventory.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by Karsten Gresch on 03.10.17.
+ */
+@Entity
+@Table(name = "PRODUCT_INVENTORY")
+public class Inventory
+{
+  @Id
+  @Column(name="ITEMID", unique=true, nullable=false)
+  private String itemId;
+
+  // length defaults to 255
+  @Column(name="LOCATION")
+  private String location;
+
+  @Column(name="QUANTITY")
+  private Integer quantity;
+
+  @Column(name="LINK")
+  private String link;
+
+
+
+}
